549368e52d4d0ef4ca1239a84f2aace8a308cfd6
Thomas <thomasfurniere@hotmail.com>
2024-03-21 15:09:27 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Windows.Themes;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfThomas1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int clicks = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            mainGrid.Background = new SolidColorBrush(Colors.Red);                    
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            clicks++;
+            List<string> buttonText = new List<string>();
+            buttonText = clicks < 8 ? new List<string> { "Almost", "Ah-Ah-Ah", "Try Again" } : new List<string> { "Almost", "magic word?", "Ah-Ah-Ah" };
+            Random random = new Random();
+
+            double maxLeftMargin = this.Width - myButton.Width;
+            double maxTopMargin = this.Height - myButton.Height;
+
+            myButton.Margin = new Thickness(random.Next(-(int)maxLeftMargin, (int)maxLeftMargin), random.Next(0, (int)maxTopMargin), 0, 0); int randomIndex = random.Next(buttonText.Count);
+            myButton.Content = buttonText[randomIndex];
+           
+            if (myButton.Content.ToString() == "magic word?")
+            {
+                myImage.Visibility = Visibility.Visible;
+                myTextBox.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                myImage.Visibility = Visibility.Collapsed;
+                myTextBox.Visibility = Visibility.Collapsed;
+            }
+
+        }
+
+        private void MyTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (myTextBox.Text == "dinosaur")
+            {
+                myImage.Visibility = Visibility.Collapsed;
+                myTextBox.Visibility = Visibility.Collapsed;
+                safeTextBlock.Visibility = Visibility.Visible;
+                mainGrid.Background = new SolidColorBrush(Colors.Green);
+                safeTextBlock.Text = "Park Security back ONLINE \nCongratulations!";
+                safeTextBlock.TextAlignment = TextAlignment.Center;
+                safeTextBlock.Padding = new Thickness(120, 120, 120, 120);  
+                safeTextBlock.Background = new SolidColorBrush(Colors.LightGreen);
+                myButton.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
